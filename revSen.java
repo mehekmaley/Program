7ea3ad3966b8d9bf3d020b05afa3a44ab6851b92
mehekmaley <31430427+mehekmaley@users.noreply.github.com>
2019-05-31 17:40:38 +0530

    Update revSen.java

--- revSen.java
+++ revSen.java
@@ -1,20 +1,60 @@
 //Program2: Reverse word in String
 
-import java.util.*;
-class revSen
+import java.util.Scanner;
+class revSen3
 {
+	public static void reverse(String sen)
+	{
+		String word="";
+		int n=sen.length()-1;
+		int f=n;
+		int k=0;
+		while(n>=0)
+		{
+			if(n>=0 && sen.charAt(n)!=' ')
+			{
+				word += sen.charAt(n);
+				n--;
+				f--;
+			}	
+			else if(sen.charAt(n)==' ' && (n==sen.length()-1))
+			{
+				while(n>=0 && sen.charAt(n)==' ')
+				{
+					n--;
+				}
+			}
+			else
+			{
+				while(n>=0 && sen.charAt(n)==' ')
+				{
+					n--;
+				}
+				reverse(word);
+				word="";
+				if(n!=-1)
+				{
+					System.out.print(" ");
+				}
+			}
+		}
+		if(f!=n)
+		{
+			reverse(word);
+			k=1;
+		}
+		if(k!=1)
+		{
+			System.out.print(word);
+		}
+	}
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Input: ");
+		System.out.print("INPUT: ");
 		String sen = sc.nextLine();
-		String[] wordarr = sen.split("\\s+");
-		System.out.print("Output: ");
-		for(int i = wordarr.length-1; i>=0 ; i--)
-		{
-			System.out.print(wordarr[i] + " ");
-		}
-		System.out.println();
+		reverse(sen);
 	}
 }
-	
+		
+		
