2f1a8f279ca1594de59827326c285011e5771463
mehekmaley <31430427+mehekmaley@users.noreply.github.com>
2019-05-31 17:50:49 +0530

    Update isValidNum.java

--- isValidNum.java
+++ isValidNum.java
@@ -11,13 +11,12 @@ class isValidNum
 		int d=0;
 		int e=0;
 		System.out.print("Input: ");
-		String input = sc.nextLine();
-		String str = input.replaceAll("\\s+","");
+		String str = sc.nextLine();
 		int n = str.length();
 		for(int i=0; i < n; i++)
 		{
 			if(str.charAt(i)=='0' || str.charAt(i)=='1' || str.charAt(i)=='2' || str.charAt(i)=='3' || str.charAt(i)=='4' || str.charAt(i)=='5' || str.charAt(i)=='6' || str.charAt(i)=='7' || 
-			str.charAt(i)=='8' || str.charAt(i)=='9' || str.charAt(i)=='e' || (str.charAt(i)=='+' && s==0) || (str.charAt(i)=='-' && s==0) || (str.charAt(i)=='.' && d==0 && e==0))
+			str.charAt(i)=='8' || str.charAt(i)=='9' || str.charAt(i)=='e' || (str.charAt(i)=='+' && s==0) || (str.charAt(i)=='-' && s==0) || (str.charAt(i)=='.' && d==0 && e==0) || str.charAt(i)==' ')
 			{ 
 				if(str.charAt(i)=='+' || str.charAt(i)=='-')
 				{
@@ -36,13 +35,13 @@ class isValidNum
 			else
 			{
 				flag = false;
-				System.out.println(input+" => "+flag);
+				System.out.println(str+" => "+flag);
 				break;
 			}
 		}
 		if(flag == true)
 		{
-			System.out.println(input+" => "+flag);
+			System.out.println(str+" => "+flag);
 		}
 	}
 }
